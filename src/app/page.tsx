ddc4d535e8b6ad029314633adff8df4590e62176
ingridcriistine <ingridrochacristine@gmail.com>
2024-09-25 14:04:19 -0300

    criando os arquivos next

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Teste Next",
+  description: "Aprendizagem técnica Bosch",
+};
+
+export default function Home() {
+  return (
+    <>
+    </>
+  );
+}
