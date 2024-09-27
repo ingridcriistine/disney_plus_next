f8ef2e40147657b6f568c0b70337eebad63246cc
ingridcriistine <ingridrochacristine@gmail.com>
2024-09-27 10:41:14 -0300

    updates

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -32,7 +32,7 @@ export default function CarouselCustomNavigation() {
 
   return (
     <Carousel
-      className="rounded-xl"
+      className="rounded-xl "
       navigation={({ setActiveIndex, activeIndex, length }) => (
         <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
           {new Array(length).fill("").map((_, i) => (
